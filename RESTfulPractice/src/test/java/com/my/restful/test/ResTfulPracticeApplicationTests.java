17db40b154dbf6971c209575c2499003ea8f69ad
sanchongdragon <qaz66223@gmail.com>
2022-06-01 14:00:45 +0800

    Create new Spring Boot + Spring Web Project

---
+++ RESTfulPractice/src/test/java/com/my/restful/test/ResTfulPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.restful.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
