48b64db3890770d35ee06b726b9e9ecb960413bc
DESKTOP-5J7K06J\57m <qaz66223@gmail.com>
2022-06-03 00:40:58 +0800

    add new book bean

---
+++ RESTfulPractice/src/main/java/com/my/restful/test/bean/Book.java
@@ -0,0 +1,26 @@
+package com.my.restful.test.bean;
+
+public class Book {
+	private String name;
+	private String isbn;
+	private int price;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+}
