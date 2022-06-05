c11f99294c7959ba28b8f3796865939f30eb7314
DESKTOP-5J7K06J\57m <qaz66223@gmail.com>
2022-06-05 18:21:20 +0800

    加入BookDAO

---
+++ RESTfulPractice/src/main/java/com/my/restful/RESTfulPractice/dao/MockBookDAO.java
@@ -0,0 +1,13 @@
+package com.my.restful.RESTfulPractice.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.my.restful.RESTfulPractice.entity.Book;
+
+@Repository
+public class MockBookDAO {
+
+	private Book book;
+	
+}
